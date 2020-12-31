2a3ef21bd6018ec717115df89959e1403ce81a4b
Shane Concannon <shaneconcannon@gmail.com>
2020-12-31 12:58:06 +0000

    Landing page build

--- frames/models.py
+++ frames/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product:
+    id : int
+    name : str
+    desc : str
+    img : str
+    price : float
--- frames/views.py
+++ frames/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Product
 
 # Create your views here.
 
 def home(request):
-	return HttpResponse("Hello World!");
+
+	prod1 = Product()
+	prod1.name = 'Christmas Bauble'
+	prod1.desc = 'A lovely Christmas Bauble'
+	prod1.price = '25.00'
+	prod1.img = 'https://scontent-dub4-1.xx.fbcdn.net/v/t1.0-9/126206158_1062800807493980_3049868636039537948_o.jpg?_nc_cat=105&ccb=2&_nc_sid=a26aad&_nc_ohc=HxHUPnj6S10AX90PM84&_nc_ht=scontent-dub4-1.xx&oh=8bcb1225c6a17be30e3ddf0ed2a64ee0&oe=6014524A'
+	
+
+	prod2 = Product()
+	prod2.name = 'Christmas Bauble'
+	prod2.desc = 'A lovely Christmas Bauble'
+	prod2.price = '25.00'
+	prod2.img = 'https://scontent-dub4-1.xx.fbcdn.net/v/t1.0-9/126206158_1062800807493980_3049868636039537948_o.jpg?_nc_cat=105&ccb=2&_nc_sid=a26aad&_nc_ohc=HxHUPnj6S10AX90PM84&_nc_ht=scontent-dub4-1.xx&oh=8bcb1225c6a17be30e3ddf0ed2a64ee0&oe=6014524A'
+
+	prod3 = Product()
+	prod3.name = 'Christmas Bauble'
+	prod3.desc = 'A lovely Christmas Bauble'
+	prod3.price = '25.00'
+	prod3.img = 'https://scontent-dub4-1.xx.fbcdn.net/v/t1.0-9/126206158_1062800807493980_3049868636039537948_o.jpg?_nc_cat=105&ccb=2&_nc_sid=a26aad&_nc_ohc=HxHUPnj6S10AX90PM84&_nc_ht=scontent-dub4-1.xx&oh=8bcb1225c6a17be30e3ddf0ed2a64ee0&oe=6014524A'
+
+	products = [prod1, prod2, prod3]
+	
+	return render(request, 'home.html', {'products': products})
