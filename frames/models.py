ab9f81cb28ef057627b4117a065540410095e330
Shane Concannon <shaneconcannon@gmail.com>
2021-01-03 14:39:22 +0000

    Login and Logout

--- frames/models.py
+++ frames/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField()
-    img = models.TextField()
+    img = models.ImageField(upload_to='pics/')
     price = models.FloatField()
