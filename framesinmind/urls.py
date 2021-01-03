a85eb0707fd712ba77d129fbe984333bf68dd159
Shane Concannon <shaneconcannon@gmail.com>
2021-01-03 15:40:41 +0000

    Fixed issue regarding to display of images

--- framesinmind/urls.py
+++ framesinmind/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls'))
 ]
 
-urlpatterms = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
